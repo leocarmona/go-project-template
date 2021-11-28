fa9981d7f7483c13fbb5ae7f40a07fd7170bf089
LeoCarmona <leocarmona_@outlook.com>
2021-11-28 03:31:05 -0300

    Improving health check performance

--- internal/app/adapters/handler/health.go
+++ internal/app/adapters/handler/health.go
@@ -3,10 +3,12 @@ package handler
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/leocarmona/go-project-template/internal/app/domain"
+	"github.com/leocarmona/go-project-template/internal/app/domain/health"
 	"github.com/leocarmona/go-project-template/internal/app/interface/presenter"
 	"github.com/leocarmona/go-project-template/internal/infra/logger"
 	"github.com/leocarmona/go-project-template/internal/infra/logger/attributes"
 	"net/http"
+	"sync"
 )
 
 type HealthHandler struct {
@@ -25,16 +27,30 @@ func (h *HealthHandler) Configure(server *echo.Echo) {
 
 func (h *HealthHandler) health(c echo.Context) error {
 	ctx := c.Request().Context()
-	read := h.services.Health.HealthReadDB(ctx)
-	write := h.services.Health.HealthWriteDB(ctx)
 
-	if read.Error != nil {
-		logger.Error(ctx, "Health check error on read database", attributes.New().WithError(read.Error))
-	}
+	var read, write *health.Health
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(2)
 
-	if write.Error != nil {
-		logger.Error(ctx, "Health check error on write database", attributes.New().WithError(write.Error))
-	}
+	go func() {
+		defer waitGroup.Done()
+
+		read = h.services.Health.HealthReadDB(ctx)
+		if read.Error != nil {
+			logger.Error(ctx, "Health check error on read database", attributes.New().WithError(read.Error))
+		}
+	}()
+
+	go func() {
+		defer waitGroup.Done()
+
+		write = h.services.Health.HealthWriteDB(ctx)
+		if write.Error != nil {
+			logger.Error(ctx, "Health check error on write database", attributes.New().WithError(write.Error))
+		}
+	}()
+
+	waitGroup.Wait()
 
 	response := presenter.HealthResponse(read, write)
 	var code int
