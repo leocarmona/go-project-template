0584db974a688377214a4f081f2298de4f16255b
LeoCarmona <leocarmona_@outlook.com>
2021-11-27 23:54:30 -0300

    Adding defer sync on log

---
+++ cmd/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+//import (
+//	"github.com/leocarmona/go-project-template/internal/app"
+//	"github.com/leocarmona/go-project-template/test/helper"
+//	"github.com/stretchr/testify/assert"
+//	"testing"
+//	"time"
+//)
+//
+//func TestShouldRunApp(t *testing.T) {
+//	helper.BuildDefaultVariables()
+//	go main()
+//	time.Sleep(100 * time.Microsecond) // must start until 1/10 millisecond
+//	assert.True(t, app.IsRunning(), "App is not running")
+//
+//	app.Close()
+//	assert.False(t, app.IsRunning(), "App is still running")
+//}
