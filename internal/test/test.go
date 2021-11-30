6fa73a73a6f069b8d61b527a8939bdd453743401
LeoCarmona <leocarmona_@outlook.com>
2021-11-30 00:10:53 -0300

    Improving test package

--- internal/test/test.go
+++ internal/test/test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"context"
 	"fmt"
 	"github.com/leocarmona/go-project-template/internal/app"
 	"github.com/leocarmona/go-project-template/internal/infra/logger"
@@ -10,6 +9,7 @@ import (
 	"gopkg.in/h2non/baloo.v3"
 	"os"
 	"os/exec"
+	"strings"
 	"testing"
 	"time"
 )
@@ -24,13 +24,23 @@ func InitLogger() {
 }
 
 func ComposeUp(t *testing.T) {
-	Shell(t, "cd ../ && make compose-up")
-	time.Sleep(1 * time.Second)
+	Shell(t, fmt.Sprintf("cd %s && make compose-up", findProjectFolder(t)))
+
+	for i := 0; i <= 30; i++ {
+		postgres, _ := ShellErr("docker ps | grep postgres")
+		redis, _ := ShellErr("docker ps | grep redis")
+
+		if strings.Contains(postgres, "healthy") &&
+			strings.Contains(redis, "healthy") {
+			break
+		}
+
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 func ComposeDown(t *testing.T) {
-	Shell(t, "cd ../ && make compose-down")
-	time.Sleep(1 * time.Second)
+	Shell(t, fmt.Sprintf("cd %s && make compose-up", findProjectFolder(t)))
 }
 
 func StartApplication() {
@@ -49,17 +59,41 @@ func Request() *baloo.Client {
 	return baloo.New("http://localhost:5000")
 }
 
-func Shell(t *testing.T, command string) {
-	logger.Info(context.Background(), fmt.Sprintf("Executing command [%s]", command), nil)
-	out, err := exec.Command("/bin/sh", "-c", command).Output()
+func Shell(t *testing.T, command string) string {
+	out, err := ShellErr(command)
 
 	if err != nil {
 		if ee, ok := err.(*exec.ExitError); ok {
-			assert.FailNow(t, string(ee.Stderr))
+			assert.FailNow(t, fmt.Sprintf("out: %s\nerr: %s", string(out), string(ee.Stderr)))
 		}
 
 		assert.FailNow(t, err.Error())
 	}
 
-	logger.Info(context.Background(), fmt.Sprintf("Response [%s]", out), nil)
+	return out
+}
+
+func ShellErr(command string) (string, error) {
+	out, err := exec.Command("/bin/sh", "-c", command).Output()
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(out), nil
+}
+
+func findProjectFolder(t *testing.T) string {
+	folders := strings.Split(Shell(t, "pwd"), "/")
+	for i := len(folders) - 1; i >= 0; i-- {
+		folder := strings.Join(folders[:i], "/")
+
+		out := Shell(t, fmt.Sprintf("ls %s", folder))
+		if strings.Contains(out, "go.mod") {
+			return folder
+		}
+	}
+
+	assert.FailNow(t, "Project folder not found")
+	return ""
 }
