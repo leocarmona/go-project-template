f271dc8daf306a6aeacd8ad75d4fb98a960fc142
LeoCarmona <leocarmona_@outlook.com>
2021-11-30 00:10:39 -0300

    Adding health handler test

---
+++ internal/test/app/adapters/handler/health_test.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"github.com/leocarmona/go-project-template/internal/test"
+	"net/http"
+	"testing"
+)
+
+func TestShouldHealth(t *testing.T) {
+	test.InitLogger()
+	test.ComposeUp(t)
+	test.StartApplication()
+
+	_ = test.Request().
+		Get("/health").
+		Expect(t).
+		Status(http.StatusOK).
+		Type("json").
+		JSON(map[string]interface{}{
+			"healthy":  true,
+			"read_db":  "UP",
+			"write_db": "UP",
+			"redis_db": "UP",
+		}).Done()
+}
